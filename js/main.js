7b574fc5aa97b72f2bba3ba53a5bcdf2892c0972
aggelosQQ <jouflis1@hotmail.com>
2017-10-20 14:46:56 +0300

    Made it more responsive

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
 		x: '0',
 	});
 	$('.menu').click(function() {
-		$('.navbar').toggleClass('navbar-visible', 1000);
-		$('.navbar').toggleClass('hide-on-mobile', 1000);
+		$('.navbar').toggleClass('navbar-visible');
+		$('.navbar').toggleClass('hide-on-mobile');
 	});
 
     $("#work").click(function (){
