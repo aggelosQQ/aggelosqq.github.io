14d49ad776fa59a97dddf9e48491dd8daab93233
aggelosQQ <jouflis1@hotmail.com>
2018-02-05 14:01:20 +0200

    Language Barrier Fix & New Recommended Way

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+
+	$('.hire-button').click(function() {
+		swal({
+			titleText: 'Add me on Discord!',
+			text: 'aggelosQQ#7499',
+			imageUrl: 'https://discordapp.com/assets/41484d92c876f76b20c7f746221e8151.svg',
+			imageWidth: 100,
+			imageAlt: 'Discord'
+		})
+	});
+
 	$('body').hide().fadeIn('slow');
 	$('.page-brand').transition({
 		x: '0',
