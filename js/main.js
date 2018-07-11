e3537c0ee86ca52a0434baeaec9ecfd332645ee6
aggelosQQ <jouflis1@hotmail.com>
2018-07-11 15:14:56 +0300

    Updates on Business Inquiries

--- js/main.js
+++ js/main.js
@@ -1,55 +1,67 @@
 $(document).ready(function() {
 
 
-	$('.hire-button').click(function() {
-		let discord_invite = "4jvCAPJ"
-		swal({
-			text: 'Use the following buttons depending on your case.',
-			icon: 'info',
-			imageAlt: 'Discord',
-			buttons: ["I joined, then what?", "Join Server" ]
-		})
-		.then((joined) => {
-			if (!joined) {
-				swal({
-					title: 'The next step is..',
-					text: 'Look for a channel called "#how-to-order" and read it carefully! Everything you need to know is there.',
-					icon: "info"
+  $('.hire-button').click(function() {
+    let discord_invite = "4jvCAPJ"
+
+    Swal({
+      title: 'Join the Server:',
+      html: '<iframe src="https://ptb.discordapp.com/widget?id=447320387408232448&theme=light" width="100%" height="350" allowtransparency="true" frameborder="0"></iframe>',
+      // type: 'warning',
+      showCancelButton: false,
+      confirmButtonText: 'Next Step...',
+    }).then((result) => {
+      if (result.value) {
+        Swal({
+          title: 'Read the Terms of Services',
+					text: 'They can either be found on #information on the Discord server, or by click on the appropriate button below.',
+					showCancelButton: true,
+					confirmButtonText: 'Terms of Service',
+					cancelButtonText: 'Next Step...'
+        }).then((result) => {
+					if (result.value) {
+						var tos = window.open('https://aggelosqq.com/terms', '_blank');
+						tos.focus();
+					} else if (result.dismiss === Swal.DismissReason.cancel) {
+						Swal({
+							title: 'You\'re almost done!',
+							html: 'Now simply, head over to <i>#general</i> and open up a ticket using <code>-new</code>',
+							confirmButtonText: 'Done!'
+						});
+					}
 				});
-			} else {
-				window.location.replace("https://discord.gg/" + discord_invite);
 			}
 		});
-	});
+});
 
-	$('body').hide().fadeIn('slow');
-	$('.page-brand').transition({
-		x: '0',
-	});
-	$('.menu').click(function() {
-		$('.navbar').toggleClass('navbar-visible');
-		$('.navbar').toggleClass('hide-on-mobile');
-	});
+$('body').hide().fadeIn('slow');
+$('.page-brand').transition({
+  x: '0',
+});
+$('.menu').click(function() {
+  $('.navbar').toggleClass('navbar-visible');
+  $('.navbar').toggleClass('hide-on-mobile');
+});
 
-    $("#work").click(function (){
-        $('html, body').animate({
-            scrollTop: $(".projects").offset().top
-        }, 1000);
-    });
+$("#work").click(function() {
+  $('html, body').animate({
+    scrollTop: $(".projects").offset().top
+  }, 1000);
+});
 
-    $("#testimonials").click(function (){
-        $('html, body').animate({
-            scrollTop: $(".testimonials").offset().top
-        }, 1000);
-    });
+$("#testimonials").click(function() {
+  $('html, body').animate({
+    scrollTop: $(".testimonials").offset().top
+  }, 1000);
+});
 
-      $('.card-outer').slick({
-          dots: true,
-          infinite: true,
-          speed: 300,
-          slidesToShow: 1,
-          adaptiveHeight: true
-      });
+$('.card-outer').slick({
+dots: true,
+infinite: true,
+speed: 300,
+slidesToShow: 1,
+adaptiveHeight: true
+});
 
 
 
